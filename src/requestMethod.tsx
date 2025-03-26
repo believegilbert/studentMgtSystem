6f21f5b8a15c133f25928d7f49ed09d88e4a31ba
Believe Gilbert <believegilbert20@gmail.com>
2025-03-26 10:01:20 +0100

    updated my api address

--- src/requestMethod.tsx
+++ src/requestMethod.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const BASE_URL = 'http://localhost:8080/api/v1/students';
-const BASE_URL = 'https://studentmanagementsystemapi.onrender/api/v1/students';
+const BASE_URL = 'https://studentmanagementsystemapi.onrender.com/api/v1/students';
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
